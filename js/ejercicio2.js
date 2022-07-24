a6178ca64ecd6d4f3433d748888dbf72014cc70e
Pereyra Jose <luispereyra14@hotmail.com>
2022-07-24 18:34:18 -0300

    ejercicio2terminado

--- js/ejercicio2.js
+++ js/ejercicio2.js
@@ -6,10 +6,28 @@
 // Escribe por pantalla el elemento que ocupa la segunda posición.
 // Sustituye el elemento que ocupa la segunda posición por la ciudad de 'Barcelona'.
 
-let ciudades = []
+let nombreCiudades = [];
+do {
+  let ciudades = prompt("Ingrese el nombre de ciudades");
+  nombreCiudades.push(ciudades);
+} while (confirm("¿Quiere escribir mas Ciudades?"));
+document.write(`<br>${nombreCiudades}</br>`)
 
-do{
-    let ciudad= prompt("ingrese una ciudad")
-    ciudades.push[ciudad]
-}while(confirm("desea ingresar otra ciudad"))
-document.write(ciudades)
+document.write(`<br>Longitud del array: ${nombreCiudades.length}</br>`)
+
+document.write
+(`
+  <br>Item 1: ${nombreCiudades[0]}</br> 
+  <br>Item 3: ${nombreCiudades[2]}</br> 
+  <br>Ultimo item: ${nombreCiudades[nombreCiudades.length - 1]}</br> 
+`)
+
+nombreCiudades.push('Paris')
+
+document.write(`<br>${nombreCiudades}</br>`)
+
+document.write(`<br>Item 2: ${nombreCiudades[1]}</br>`
+)
+nombreCiudades[1] = 'Barcelona'
+
+document.write(`<br>${nombreCiudades}</br>`)
